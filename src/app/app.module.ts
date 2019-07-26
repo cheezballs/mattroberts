b5f72aecd395b631470ffc23f75510c2c56cc85a
Matt Roberts <sweetcrapmatt@gmail.com>
2019-07-26 12:20:08 -0500

    Navbar, styling, collapsing, responsiveness, etc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, NgbModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, PageNotFoundComponent],
+  imports: [BrowserModule, NgbModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+
+    ]),
+  ]
 })
 export class HeaderComponent implements OnInit {
+  showMobileNavbar = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  toggleMobileNavbar(): void {
+    this.showMobileNavbar = !this.showMobileNavbar;
   }
-
 }
