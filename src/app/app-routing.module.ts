16f32788ba8d4de10b693ed3a0ddaba680b69ac1
Matt Roberts <sweetcrapmatt@gmail.com>
2019-07-28 19:01:51 -0500

    Stubbig out more pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
-
+import { GuidesComponent } from './guides/guides.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'guides', component: GuidesComponent },
+  { path: 'projects', component: ProjectsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
+import { GuidesComponent } from './guides/guides.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, PageNotFoundComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, PageNotFoundComponent, GuidesComponent, ProjectsComponent],
   imports: [BrowserModule, NgbModule, BrowserAnimationsModule, FontAwesomeModule, AppRoutingModule ],
   providers: [],
   bootstrap: [AppComponent]
